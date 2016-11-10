8f4bfbc591425473d7fd3cfa478069ac3c956dd9
Haroon Choudery <haroon.choudery@gmail.com>
2016-11-10 11:12:06 -0800

    test

--- psycopg-sample.py
+++ psycopg-sample.py
@@ -9,6 +9,7 @@
 import psycopg2
 
 conn = psycopg2.connect(database="postgres", user="postgres", password="pass", host="localhost", port="5432")
+conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 
 dbname = "Tcount"
 #Create a Table
