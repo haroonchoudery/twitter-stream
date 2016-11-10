23fefa9eec0fe3a8559d09f8a0f16f6902433c1e
Haroon Choudery <haroon.choudery@gmail.com>
2016-11-10 11:53:41 -0800

    changed wordcount

--- tweetwordcount/src/bolts/wordcount.py
+++ tweetwordcount/src/bolts/wordcount.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, print_function, unicode_literals
 from collections import Counter
 from streamparse.bolt import Bolt
 import psycopg2
-
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
 conn = psycopg2.connect(database="postgres", user="postgres", password="pass", host="localhost", port="5432")
 conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
