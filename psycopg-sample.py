798f47423ef7c3e2689f755b503bc7e17b758afd
Haroon Choudery <haroon.choudery@gmail.com>
2016-11-10 11:12:37 -0800

    test

--- psycopg-sample.py
+++ psycopg-sample.py
@@ -5,8 +5,8 @@
 #Connecting to a database
 #Note: If the database does not exist, then this command will create the database
 
-
 import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
 conn = psycopg2.connect(database="postgres", user="postgres", password="pass", host="localhost", port="5432")
 conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
