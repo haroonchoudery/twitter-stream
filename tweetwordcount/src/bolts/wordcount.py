4f2b13e33177f7faa31a912fb38551273ba2cb22
Haroon Choudery <haroon.choudery@gmail.com>
2016-11-10 11:45:16 -0800

    test

--- tweetwordcount/src/bolts/wordcount.py
+++ tweetwordcount/src/bolts/wordcount.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, print_function, unicode_literals
 from collections import Counter
 from streamparse.bolt import Bolt
 
-
+cur = conn.cursor()
 
 class WordCounter(Bolt):
 
@@ -20,6 +20,20 @@ class WordCounter(Bolt):
         # Table name: Tweetwordcount 
         # you need to create both the database and the table in advance.
         
+        if (word in Counter) == False:
+            cur.execute("INSERT INTO Tweetwordcount (word,count) \
+                  VALUES (word, 1)");
+            conn.commit()
+        else:
+            cur.execute("UPDATE Tweetwordcount SET count=%s WHERE word=%s", (count+1, word))
+            conn.commit()
+        #Update
+        #Assuming you are passing the tuple (uWord, uCount) as an argument
+        
+        
+
+        
+
 
         # Increment the local count
         self.counts[word] += 1
@@ -27,3 +41,5 @@ class WordCounter(Bolt):
 
         # Log the count - just to see the topology running
         self.log('%s: %d' % (word, self.counts[word]))
+
+conn.close()
