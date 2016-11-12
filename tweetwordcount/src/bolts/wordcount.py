74b7f48558681e00d6af5939ecc4677ae1874a66
Haroon Choudery <haroon.choudery@gmail.com>
2016-11-11 17:51:48 -0800

    test

--- tweetwordcount/src/bolts/wordcount.py
+++ tweetwordcount/src/bolts/wordcount.py
@@ -5,13 +5,13 @@ from streamparse.bolt import Bolt
 import psycopg2
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
-conn = psycopg2.connect(database="postgres", user="postgres", password="pass", host="localhost", port="5432")
+conn = psycopg2.connect(database="Tcount", user="postgres", password="pass", host="localhost", port="5432")
 conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 #Create a Table
 #The first step is to create a cursor. 
 cur = conn.cursor()
-cur.execute('CREATE DATABASE IF NOT EXISTS Tcount')
-cur.execute('''CREATE TABLE IF NOT EXISTS Tweetwordcount
+# cur.execute('CREATE DATABASE Tcount')
+cur.execute('''CREATE TABLE Tweetwordcount
     (word TEXT PRIMARY KEY     NOT NULL,
     count INT     NOT NULL);''')
 conn.commit()
