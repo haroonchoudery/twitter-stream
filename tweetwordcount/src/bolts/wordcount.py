264ef5af32b20e32ddaea83a6962329c354bdf5a
Haroon Choudery <Haroon@Haroons-MacBook-Pro.local>
2016-11-19 13:34:39 -0800

    changed names of database and table

--- tweetwordcount/src/bolts/wordcount.py
+++ tweetwordcount/src/bolts/wordcount.py
@@ -5,13 +5,15 @@ from streamparse.bolt import Bolt
 import psycopg2
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
-conn = psycopg2.connect(database="postgres", user="postgres", password="pass", host="localhost", port="5432")
+conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
 conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-# #The first step is to create a cursor. 
+
+# The first step is to create a cursor. 
 cur = conn.cursor()
 
-dbname = 'tcount'
-cur.execute('CREATE DATABASE ' + dbname)
+# Code to create Database - only run first time
+# dbname = 'tcount'
+# cur.execute('CREATE DATABASE ' + dbname)
 
 # cur.execute('DROP DATABASE IF EXISTS Tcount')
 # cur.execute('CREATE DATABASE Tcount')
