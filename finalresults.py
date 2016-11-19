bc3ee44950ccbb8a6c1d229fe14a822166acae65
Haroon Choudery <Haroon@Haroons-MacBook-Pro.local>
2016-11-19 14:08:15 -0800

    finalresults file

---
+++ finalresults.py
@@ -0,0 +1,19 @@
+from __future__ import absolute_import, print_function, unicode_literals
+
+from collections import Counter
+from streamparse.bolt import Bolt
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
+conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+
+cur = conn.cursor()
+
+if __name__ == "__main__":
+	argword = sys.argv[1]
+
+	cur.execute('SELECT COUNT(*) FROM Tweetwordcount WHERE word = argword;')
+
+
+conn.close()
