8357ec96a070e9ce66c83fc98364911e3451cc9d
Haroon Choudery <Haroon@Haroons-MacBook-Pro.local>
2016-11-19 15:24:40 -0800

    finalresults file

--- finalresults.py
+++ finalresults.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
 	else:
 		argword = None
 
-	cur.execute("SELECT word, count from Tweetwordcount WHERE word ~ '[^0-9]'")
+	cur.execute("SELECT word, count from Tweetwordcount WHERE word ~ '[^\d\W]+'")
 	records = cur.fetchall()
 
 	if not argword:
