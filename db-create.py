d77d158960b2041368b46d9585e35d458d572762
Haroon Choudery <Haroon@Haroons-MacBook-Pro.local>
2016-11-20 13:02:25 -0800

    added changes

--- db-create.py
+++ db-create.py
@@ -4,6 +4,6 @@ from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 conn = psycopg2.connect(user="postgres", password="pass", host="localhost", port="5432")
 conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 cur = conn.cursor()
-conn.execute('CREATE DATABASE tcount')
+cur.execute('CREATE DATABASE tcount')
 conn.commit()
 conn.close()
